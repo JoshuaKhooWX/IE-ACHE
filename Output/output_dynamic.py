0992a5a8502f93a23080cfe8fcfbff8734f292a8
JoshuaKhooWX <73164041+JoshuaKhooWX@users.noreply.github.com>
2021-06-24 13:42:56 +0800

    Update output_dynamic.py

--- Output/output_dynamic.py
+++ Output/output_dynamic.py
@@ -38,6 +38,7 @@ def generateMd5(file_name):
 
     
 def dragonfly():
+    start = time.perf_counter()
     # Request Keygen to initiate Dragonfly SAE
     print("Starting Dragonfly Key Exchange ")
 
@@ -1055,7 +1056,6 @@ own_ipaddr = '192.168.0.4'
 own_address_kg = (own_ipaddr, 4380)
 
 print("Hello!")
-start = time.perf_counter()
 x = 1
 while (x != 0):
     expr = input("Enter an expression using letters (A, B, C) for clients and symbols ( +, -, *) for operators. [E.g. A + B * C]: ")
